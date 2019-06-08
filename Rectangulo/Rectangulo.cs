d7bae4f3ac5bac3c5c91054674bb1eeb45485d5c
JoaquinHN <dannycantarero3@gmail.com>
2019-06-08 14:58:34 -0600

    AgregandoINotifyPropertyChanged

---
+++ Rectangulo/Rectangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+namespace Rectangulo
+{
+    class Rectangulo: INotifyPropertyChanged
+    {
+        private double distancia;
+        private double area;
+        private double baseR;
+        private double altura;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+    }
+}
